4a1b20b42e4db3f19aa815c21e9fb5a085c923d4
Lightfield0 <lightfield33@gmail.com>
2024-02-28 22:10:54 +0300

    Changes to be committed: 	modified:   LeBergerCapital/__pycache__/settings.cpython-39.pyc 	modified:   LeBergerCapital/__pycache__/urls.cpython-39.pyc 	modified:   LeBergerCapital/urls.py

--- LeBergerCapital/urls.py
+++ LeBergerCapital/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('questionnaire/', include('question.urls')),  # Burası question uygulamanız için
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
